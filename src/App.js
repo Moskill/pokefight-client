bdc84575117049bf8ae198fa995d43898b6cc806
unknown <cborek@web.de>
2021-11-03 23:14:14 +0100

    Created the play card incl. CSS styles. Added several favicons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import SingleCard from './components/cards/SingleCard';
 
 function App() {
+
+  const [pokemonList, setPokemonList] = useState();
+
+  // ALle Pokemons fetchen 
+  useEffect(() => {
+    axios.get('http://localhost:8000/pokemon')
+    .then(response => {
+      setPokemonList(response.data);
+    }, error => {
+      console.log(error);
+    });
+  }, [])
+
+  const getRandNo = () => {
+    let randNo = Math.floor(Math.random() * 809);
+    return randNo;
+  }
+    
   return (
-    <div className="App">
-      <h1>Pokemon Deathmatch </h1>
+    <div className="app">
+      <div className="table">
+        <SingleCard cardNo={getRandNo()} props={pokemonList}/>
+      </div>
     </div>
   );
 }
